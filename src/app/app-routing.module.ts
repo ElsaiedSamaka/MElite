204898153d82d5d06fa82801d9ebef7d8142521e
Saied <elsaied.samaka1997@gmail.com>
2023-04-22 18:30:43 +0200

    setup AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/core/guards/AuthGaurd.guard';
 
 const routes: Routes = [
   {
     path: 'inbox',
+    canLoad: [AuthGuard],
     loadChildren: () =>
       import('./pages/inbox/inbox.module').then((m) => m.InboxModule),
   },
   {
     path: 'index',
-    // canLoad:[],
+    canLoad: [AuthGuard],
     loadChildren: () =>
       import('./pages/index/index.module').then((m) => m.IndexModule),
   },
