9c28404e58c8c1d558f0d0d5039ec96c2bea00eb
Saied <elsaied.samaka1997@gmail.com>
2023-04-22 18:45:00 +0200

    setup signout logic

--- src/app/pages/auth/components/signout/signout.component.ts
+++ src/app/pages/auth/components/signout/signout.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/core/services/auth.service';
 
 @Component({
   selector: 'app-signout',
   templateUrl: './signout.component.html',
-  styleUrls: ['./signout.component.css']
+  styleUrls: ['./signout.component.css'],
 })
 export class SignoutComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private authService: AuthService, private router: Router) {}
   ngOnInit() {
+    this.authService.signout().subscribe(() => {
+      this.router.navigateByUrl('/');
+    });
   }
-
 }
