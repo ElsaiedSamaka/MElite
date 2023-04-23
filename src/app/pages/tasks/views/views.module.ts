35d2c7c38eab7c5ba563e39b9bb75e90301fcd61
Saied <elsaied.samaka1997@gmail.com>
2023-04-23 09:36:32 +0200

    data grid basic setup

--- src/app/pages/tasks/views/views.module.ts
+++ src/app/pages/tasks/views/views.module.ts
@@ -1,12 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { DataGridComponent } from './data-grid/data-grid.component';
+import { PaginationComponent } from './pagination/pagination.component';
 import { TasksCreateComponent } from './tasks-create/tasks-create.component';
 import { TasksDeleteComponent } from './tasks-delete/tasks-delete.component';
 
 @NgModule({
   imports: [CommonModule, SharedModule],
-  declarations: [TasksCreateComponent, TasksDeleteComponent],
-  exports: [TasksCreateComponent, TasksDeleteComponent],
+  declarations: [
+    TasksCreateComponent,
+    TasksDeleteComponent,
+    DataGridComponent,
+    PaginationComponent,
+  ],
+  exports: [
+    TasksCreateComponent,
+    TasksDeleteComponent,
+    DataGridComponent,
+    PaginationComponent,
+  ],
 })
 export class ViewsModule {}
