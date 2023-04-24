aa32cc1f770d70bce6e22814d01663fa55814bc4
Saied <elsaied.samaka1997@gmail.com>
2023-04-24 11:35:52 +0200

    styling not found page

--- src/app/pages/inbox/inbox.module.ts
+++ src/app/pages/inbox/inbox.module.ts
@@ -1,12 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { DetailedComponent } from './components/detailed/detailed.component';
 import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { PlaceholderComponent } from './components/placeholder/placeholder.component';
 import { InboxRoutingModule } from './inbox-routing.module';
 import { ViewsModule } from './views/views.module';
 
 @NgModule({
   imports: [InboxRoutingModule, CommonModule, SharedModule, ViewsModule],
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent,
+    NotFoundComponent,
+    PlaceholderComponent,
+    DetailedComponent,
+  ],
 })
 export class InboxModule {}
