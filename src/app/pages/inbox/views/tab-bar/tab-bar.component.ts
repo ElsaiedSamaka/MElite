0477a446896471e448a39024df59e7aa5b17d32d
Saied <elsaied.samaka1997@gmail.com>
2023-04-26 17:13:19 +0200

    inbox tabs

--- src/app/pages/inbox/views/tab-bar/tab-bar.component.ts
+++ src/app/pages/inbox/views/tab-bar/tab-bar.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tab-bar',
   templateUrl: './tab-bar.component.html',
-  styleUrls: ['./tab-bar.component.css']
+  styleUrls: ['./tab-bar.component.css'],
 })
 export class TabBarComponent implements OnInit {
+  activeTab = 'خطاب تغطية';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  openExternalLetterTab() {
+    this.activeTab = 'خطاب تغطية';
   }
-
+  openOfferTab() {
+    this.activeTab = 'عروض';
+  }
+  openExplanationsTab() {
+    this.activeTab = 'الشروحات';
+  }
+  openMemoTab() {
+    this.activeTab = 'مذكرة';
+  }
+  openAttachmentTab() {
+    this.activeTab = 'المرفقات';
+  }
+  openCommitteeMinutesTab() {
+    this.activeTab = 'محاضر اللجان';
+  }
 }
