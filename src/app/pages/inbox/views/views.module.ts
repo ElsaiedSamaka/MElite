a5a9fda60223645e065a040a525c17b73d737ff2
Saied <elsaied.samaka1997@gmail.com>
2023-04-26 16:21:12 +0200

    inbox tabs

--- src/app/pages/inbox/views/views.module.ts
+++ src/app/pages/inbox/views/views.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FilterBarComponent } from './filter-bar/filter-bar.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { TabBarComponent } from './tab-bar/tab-bar.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [FilterBarComponent, SearchBarComponent],
-  exports: [FilterBarComponent, SearchBarComponent],
+  declarations: [FilterBarComponent, SearchBarComponent, TabBarComponent],
+  exports: [FilterBarComponent, SearchBarComponent, TabBarComponent],
 })
 export class ViewsModule {}
