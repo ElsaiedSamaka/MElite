051c70dfe0e307363d989ad2da058a8c368f777b
Saied <elsaied.samaka1997@gmail.com>
2023-04-26 14:30:46 +0200

    select tabs

--- src/app/pages/inbox/views/tab-bar/tab-bar.component.ts
+++ src/app/pages/inbox/views/tab-bar/tab-bar.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tab-bar',
   templateUrl: './tab-bar.component.html',
-  styleUrls: ['./tab-bar.component.css']
+  styleUrls: ['./tab-bar.component.css'],
 })
 export class TabBarComponent implements OnInit {
+  tabs = [
+    { id: 1, label: 'الجميع' },
+    { id: 2, label: 'الجديد' },
+    { id: 3, label: 'المؤجل' },
+    { id: 4, label: 'عاجلة' },
+    { id: 5, label: 'للتوقيع' },
+    { id: 6, label: 'الطلبات' },
+  ];
+  selectedTabId = 1;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  selectTab(id) {
+    this.selectedTabId = id;
   }
-
 }
