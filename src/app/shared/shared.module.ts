8d1dae32e88b5241fdfe50c3434eec95a4b26aad
Saied <elsaied.samaka1997@gmail.com>
2023-04-26 20:02:29 +0200

    Breakpoint

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
 import { InputComponent } from './input/input.component';
@@ -11,7 +12,7 @@ import { SortIconComponent } from './sort-icon/sort-icon.component';
 import { ToastComponent } from './toast/toast.component';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   declarations: [
     BreadcrumbComponent,
     InputComponent,
