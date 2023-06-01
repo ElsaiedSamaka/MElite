344e706f8faa40f5f9ab5d0bb3ff7ced05279f1b
Saied <elsaied.samaka1997@gmail.com>
2023-06-01 17:05:18 +0300

    breakingpoint

--- src/app/pages/checkout/components/index/index.component.ts
+++ src/app/pages/checkout/components/index/index.component.ts
@@ -1,15 +1,78 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/core/services/cart.service';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.css'],
 })
 export class IndexComponent implements OnInit {
+  cartItems;
 
-  constructor() { }
+  constructor(private cartService: CartService) {}
 
   ngOnInit() {
+    this.getCartItems();
   }
-
+  getCartItems(): void {
+    this.cartService.getAll().subscribe({
+      next: (cartItems) => {
+        this.cartItems = this.cartService.items$.value;
+      },
+      error: (err) => {
+        console.log('error while returning cart item', err);
+      },
+      complete: () => {},
+    });
+  }
+  deleteCartItem(id: string): void {
+    this.cartService.deleteById(id).subscribe({
+      next: (deletedCartItem) => {
+        this.cartItems = this.cartService.items$.value;
+      },
+      error: (err) => {
+        console.log('error while deleteCartItem', err);
+      },
+      complete: () => {},
+    });
+  }
+  updateCartItem(id: string, cartItem: any): void {
+    this.cartService.put(id, cartItem).subscribe({
+      next: (updatedCartItem) => {},
+      error: (err) => {
+        console.log('error while updateCartItem', err);
+      },
+      complete: () => {},
+    });
+  }
+  increaseCartItemQty(id: string, cartItem: any): void {
+    const updatedCartItem = {
+      ...cartItem,
+      quantity: cartItem.quantity + 1,
+    };
+    this.cartService.put(id, updatedCartItem).subscribe({
+      next: (updatedCartItem) => {
+        this.cartItems = this.cartService.items$.value;
+      },
+      error: (err) => {
+        console.log('error while updateCartItem', err);
+      },
+      complete: () => {},
+    });
+  }
+  decreaseCartItemQty(id: string, cartItem: any): void {
+    const updatedCartItem = {
+      ...cartItem,
+      quantity: cartItem.quantity - 1,
+    };
+    this.cartService.put(id, updatedCartItem).subscribe({
+      next: (updatedCartItem) => {
+        this.cartItems = this.cartService.items$.value;
+      },
+      error: (err) => {
+        console.log('error while updateCartItem', err);
+      },
+      complete: () => {},
+    });
+  }
 }
