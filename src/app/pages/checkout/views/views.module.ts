71dabe376a9163a5215c0fc51517cd172dff4124
Saied <elsaied.samaka1997@gmail.com>
2023-06-10 18:47:16 +0300

    finally 54

--- src/app/pages/checkout/views/views.module.ts
+++ src/app/pages/checkout/views/views.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { AddressCartComponent } from './address-cart/address-cart.component';
 import { PayementDatailsComponent } from './payement-datails/payement-datails.component';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, SharedModule, ReactiveFormsModule],
   declarations: [PayementDatailsComponent, AddressCartComponent],
   exports: [PayementDatailsComponent, AddressCartComponent],
 })
