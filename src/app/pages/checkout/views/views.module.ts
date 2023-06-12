0b3947a9e4da2d051d72dcd5d81c2340c884fe39
Saied <elsaied.samaka1997@gmail.com>
2023-06-12 10:08:20 +0300

    finally 62

--- src/app/pages/checkout/views/views.module.ts
+++ src/app/pages/checkout/views/views.module.ts
@@ -3,19 +3,10 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AddressCartComponent } from './address-cart/address-cart.component';
 import { PayementCardComponent } from './payement-card/payement-card.component';
-import { PayementDatailsComponent } from './payement-datails/payement-datails.component';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule],
-  declarations: [
-    PayementDatailsComponent,
-    AddressCartComponent,
-    PayementCardComponent,
-  ],
-  exports: [
-    PayementDatailsComponent,
-    AddressCartComponent,
-    PayementCardComponent,
-  ],
+  declarations: [AddressCartComponent, PayementCardComponent],
+  exports: [AddressCartComponent, PayementCardComponent],
 })
 export class ViewsModule {}
