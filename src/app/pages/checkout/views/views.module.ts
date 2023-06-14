9e687b821276e59250b6103517e05465487738df
Saied <elsaied.samaka1997@gmail.com>
2023-06-14 23:53:23 +0300

    finally 97

--- src/app/pages/checkout/views/views.module.ts
+++ src/app/pages/checkout/views/views.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AddressCartComponent } from './address-cart/address-cart.component';
 import { CheckoutCartComponent } from './checkout-cart/checkout-cart.component';
@@ -8,7 +9,7 @@ import { PayementCardComponent } from './payement-card/payement-card.component';
 import { StepperComponent } from './stepper/stepper.component';
 
 @NgModule({
-  imports: [CommonModule, SharedModule, ReactiveFormsModule],
+  imports: [CommonModule, RouterModule, SharedModule, ReactiveFormsModule],
   declarations: [
     AddressCartComponent,
     PayementCardComponent,
