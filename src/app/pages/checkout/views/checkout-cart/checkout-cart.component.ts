6b2ace34d5e7c3fd5779093135bd9a6d386db2e9
Saied <elsaied.samaka1997@gmail.com>
2023-06-14 12:48:31 +0300

    finally 90

---
+++ src/app/pages/checkout/views/checkout-cart/checkout-cart.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/core/services/cart.service';
+
+@Component({
+  selector: 'app-checkout-cart',
+  templateUrl: './checkout-cart.component.html',
+  styleUrls: ['./checkout-cart.component.css'],
+})
+export class CheckoutCartComponent implements OnInit {
+  cartItems: any[] = [];
+  constructor(private cartService: CartService) {}
+
+  ngOnInit() {
+    this.cartItems = this.cartService.items$.value;
+  }
+  getCartItems(): void {
+    this.cartService.getUserCarts().subscribe({
+      next: (cartItems) => {
+        this.cartItems = this.cartService.items$.value;
+      },
+      error: (err) => {
+        console.log('error while returning cart item', err);
+      },
+      complete: () => {},
+    });
+  }
+  deleteCartItem(id: string): void {
+    this.cartService.deleteById(id).subscribe({
+      next: (deletedCartItem) => {
+        this.cartItems = this.cartService.items$.value;
+      },
+      error: (err) => {
+        console.log('error while deleteCartItem', err);
+      },
+      complete: () => {},
+    });
+  }
+  updateCartItem(id: string, cartItem: any): void {
+    this.cartService.put(id, cartItem).subscribe({
+      next: (updatedCartItem) => {},
+      error: (err) => {
+        console.log('error while updateCartItem', err);
+      },
+      complete: () => {},
+    });
+  }
+  increaseCartItemQty(id: string, cartItem: any): void {
+    const updatedCartItem = {
+      ...cartItem,
+      quantity: cartItem.quantity + 1,
+      price: cartItem.product.price * (cartItem.quantity + 1),
+    };
+    this.cartService.put(id, updatedCartItem).subscribe({
+      next: (updatedCartItem) => {
+        this.cartItems = this.cartService.items$.value;
+      },
+      error: (err) => {
+        console.log('error while updateCartItem', err);
+      },
+      complete: () => {},
+    });
+  }
+  decreaseCartItemQty(id: string, cartItem: any): void {
+    const updatedCartItem = {
+      ...cartItem,
+      quantity: cartItem.quantity - 1,
+      price: cartItem.price - cartItem.product.price,
+    };
+
+    this.cartService.put(id, updatedCartItem).subscribe({
+      next: (updatedCartItem) => {
+        this.cartItems = this.cartService.items$.value;
+      },
+      error: (err) => {
+        console.log('error while updateCartItem', err);
+      },
+      complete: () => {},
+    });
+  }
+}
--- src/app/pages/checkout/views/views.module.ts
+++ src/app/pages/checkout/views/views.module.ts
@@ -3,12 +3,23 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AddressCartComponent } from './address-cart/address-cart.component';
+import { CheckoutCartComponent } from './checkout-cart/checkout-cart.component';
 import { PayementCardComponent } from './payement-card/payement-card.component';
 import { StepperComponent } from './stepper/stepper.component';
 
 @NgModule({
   imports: [CommonModule, SharedModule, ReactiveFormsModule],
-  declarations: [AddressCartComponent, PayementCardComponent, StepperComponent],
-  exports: [AddressCartComponent, PayementCardComponent, StepperComponent],
+  declarations: [
+    AddressCartComponent,
+    PayementCardComponent,
+    CheckoutCartComponent,
+    StepperComponent,
+  ],
+  exports: [
+    AddressCartComponent,
+    PayementCardComponent,
+    StepperComponent,
+    CheckoutCartComponent,
+  ],
 })
 export class ViewsModule {}
