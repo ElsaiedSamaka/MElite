9a5973837ec1405e257185a8c2ae9d88bbf00665
Saied <elsaied.samaka1997@gmail.com>
2023-06-17 01:36:23 +0300

    finally 99

--- src/app/pages/index/views/product-reviews/product-reviews.component.ts
+++ src/app/pages/index/views/product-reviews/product-reviews.component.ts
@@ -8,7 +8,17 @@ import { ReviewsService } from 'src/core/services/reviews.service';
 })
 export class ProductReviewsComponent implements OnInit {
   @Input() reviews: any[] = [];
+  sum: number = 0;
+  average: number = 0;
+  ratedStars: any[] = [];
+  unratedStars: any[] = [];
   constructor(private reviewsService: ReviewsService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.sum = this.reviews.reduce((acc, review) => acc + review.rating, 0);
+    this.average = this.sum / this.reviews.length;
+    this.ratedStars = Array.from({ length: this.average }, (_, i) => i + 1);
+    this.unratedStars = [...Array(5 - this.ratedStars.length).keys()];
+    console.log(this.ratedStars);
+  }
 }
