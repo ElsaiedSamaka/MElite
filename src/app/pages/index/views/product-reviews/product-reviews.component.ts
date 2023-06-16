7c5a6cd9e4bf4f819590f2a432706f3973cb1794
Saied <elsaied.samaka1997@gmail.com>
2023-06-16 20:56:06 +0300

    finally 99

--- src/app/pages/index/views/product-reviews/product-reviews.component.ts
+++ src/app/pages/index/views/product-reviews/product-reviews.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
 import { ReviewsService } from 'src/core/services/reviews.service';
 
 @Component({
@@ -12,17 +11,4 @@ export class ProductReviewsComponent implements OnInit {
   constructor(private reviewsService: ReviewsService) {}
 
   ngOnInit() {}
-  reviewForm = new FormGroup({
-    rating: new FormControl(''),
-    review: new FormControl(''),
-  });
-  postReview(review: any): void {
-    // this.reviewsService.post()
-  }
-  updatedReview(id: string, review: any): void {
-    // this.reviewsService.put()
-  }
-  deleteReview(id: string): void {
-    // this.reviewsService.deleteById(id)
-  }
 }
