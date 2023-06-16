926eb16163afba8f3b2a74cdc9010576f341c5fa
Saied <elsaied.samaka1997@gmail.com>
2023-06-16 09:53:59 +0300

    finally 99

--- src/app/pages/index/views/views.module.ts
+++ src/app/pages/index/views/views.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ProductCardComponent } from './product-card/product-card.component';
+import { ProductReviewsComponent } from './product-reviews/product-reviews.component';
 
 @NgModule({
   imports: [CommonModule, SharedModule, RouterModule],
-  declarations: [ProductCardComponent],
-  exports: [ProductCardComponent],
+  declarations: [ProductCardComponent, ProductReviewsComponent],
+  exports: [ProductCardComponent, ProductReviewsComponent],
 })
 export class IndexViewsModule {}
