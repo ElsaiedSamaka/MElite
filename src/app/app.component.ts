d6e7470475af02cfe1025196d72f0663d5476311
Saied <elsaied.samaka1997@gmail.com>
2023-06-17 21:36:04 +0300

    okay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { AuthService } from 'src/core/services/auth.service';
+import { UsersService } from 'src/core/services/users.service';
 import { LoadingService } from './shared/services/loading.service';
 
 @Component({
@@ -11,16 +12,33 @@ import { LoadingService } from './shared/services/loading.service';
 export class AppComponent implements OnInit {
   signedin$: BehaviorSubject<boolean>;
   loading$: Observable<boolean>;
-
+  user: any = {
+    username: 'username',
+    email: 'lorem@example.com',
+    phonenumber: '011-50567130',
+    role: {
+      name: 'user',
+    },
+  };
   constructor(
     private authService: AuthService,
-    private loadingService: LoadingService
+    private loadingService: LoadingService,
+    private userService: UsersService
   ) {
     this.signedin$ = authService.signedin$;
     this.loading$ = loadingService.loading$;
-    console.log(authService.signedin$.value);
   }
   ngOnInit() {
     this.authService.checkAuth().subscribe(() => {});
+    this.user = this.authService.USER$.value;
+    this.userService.getUser().subscribe({
+      next: (user) => {
+        this.user = user;
+      },
+      error: (err) => {
+        console.log('error while returning current user', err);
+      },
+      complete: () => {},
+    });
   }
 }
