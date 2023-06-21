a814894ce3e99a8aff46ef0e81bb3756b5f1c115
Saied <elsaied.samaka1997@gmail.com>
2023-06-22 00:20:29 +0300

    okay

--- src/app/pages/auth/components/signin/signin.component.ts
+++ src/app/pages/auth/components/signin/signin.component.ts
@@ -43,7 +43,9 @@ export class SigninComponent implements OnInit {
     this.authService
       .signin(this.authForm.value.email!, this.authForm.value.password!)
       .subscribe({
-        next: (res) => {},
+        next: (response) => {
+          localStorage.setItem('token', response.token);
+        },
         error: (err) => {
           if (!err.status) {
             this.authForm.setErrors({ noConnection: true });
