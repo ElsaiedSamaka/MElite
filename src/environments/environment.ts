14b7573a71b4d227eb144d60e8dbe8d87657744d
Saied <elsaied.samaka1997@gmail.com>
2023-06-22 00:23:14 +0300

    okay

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // api_url: 'https://void-backend.onrender.com',
-  api_url: 'http://localhost:3000',
+  api_url: 'https://void-backend.onrender.com',
+  // api_url: 'http://localhost:3000',
 };
